88363bad80b790f2eb5d4114c5039af62f5a43c5
Avik Sengupta <avik@sengupta.net>
2017-05-07 02:31:30 +0100

    Use wildcard classpath, supported in new JavaCall

--- src/Taro.jl
+++ src/Taro.jl
@@ -6,32 +6,11 @@ using DataArrays
 using Compat
 
 tika_jar = joinpath(dirname(@__FILE__), "..", "deps", "tika-app-1.10.jar")
-avalon_jar = joinpath(dirname(@__FILE__), "..", "deps", "fop-2.0", "lib", "avalon-framework-4.2.0.jar")
-batik_jar = joinpath(dirname(@__FILE__), "..", "deps", "fop-2.0", "lib", "batik-all-1.8.jar")
-commons_io_jar = joinpath(dirname(@__FILE__), "..", "deps", "fop-2.0", "lib", "commons-io-1.3.1.jar")
-commons_logging_jar = joinpath(dirname(@__FILE__), "..", "deps", "fop-2.0", "lib", "commons-logging-1.0.4.jar")
-fontbox_jar = joinpath(dirname(@__FILE__), "..", "deps", "fop-2.0", "lib", "fontbox-1.8.5.jar")
-serializer_jar = joinpath(dirname(@__FILE__), "..", "deps", "fop-2.0", "lib", "serializer-2.7.0.jar")
-xalan_jar = joinpath(dirname(@__FILE__), "..", "deps", "fop-2.0", "lib", "xalan-2.7.0.jar")
-xerces_jar = joinpath(dirname(@__FILE__), "..", "deps", "fop-2.0", "lib", "xercesImpl-2.7.1.jar")
-xml_apis_jar = joinpath(dirname(@__FILE__), "..", "deps", "fop-2.0", "lib", "xml-apis-1.3.04.jar")
-xml_apis_ext_jar = joinpath(dirname(@__FILE__), "..", "deps", "fop-2.0", "lib", "xml-apis-ext-1.3.04.jar")
-xmlgraphics_common_jar = joinpath(dirname(@__FILE__), "..", "deps", "fop-2.0", "lib", "xmlgraphics-commons-2.0.1.jar")
-
+fop_lib = joinpath(dirname(@__FILE__), "..", "deps", "fop-2.0", "lib", "*")
 fop_jar = joinpath(dirname(@__FILE__), "..", "deps", "fop-2.0", "build", "fop.jar")
 
 JavaCall.addClassPath(tika_jar)
-JavaCall.addClassPath(avalon_jar)
-JavaCall.addClassPath(batik_jar)
-JavaCall.addClassPath(commons_io_jar)
-JavaCall.addClassPath(commons_logging_jar)
-JavaCall.addClassPath(fontbox_jar)
-JavaCall.addClassPath(serializer_jar)
-JavaCall.addClassPath(xalan_jar)
-JavaCall.addClassPath(xerces_jar)
-JavaCall.addClassPath(xml_apis_jar)
-JavaCall.addClassPath(xml_apis_ext_jar)
-JavaCall.addClassPath(xmlgraphics_common_jar)
+JavaCall.addClassPath(fop_lib)
 JavaCall.addClassPath(fop_jar)
 
 
