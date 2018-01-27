b14b665a7995aec8a59a6f4500aa87fefa3878c1
randyzwitch <rzwitch@gmail.com>
2018-01-27 18:56:20 -0500

    Update jars, fix link rot
    Update jars to the newest ones online, fix documentation where
    DataFrames moved to JuliaData.

--- deps/build.jl
+++ deps/build.jl
@@ -1,15 +1,15 @@
 tdeps = dirname(@__FILE__)
-tika_jar = joinpath(tdeps, "tika-app-1.10.jar")
+tika_jar = joinpath(tdeps, "tika-app-1.17.jar")
 if !isfile(tika_jar)
-    info("  Downloading tika-app-1.10.jar from Maven Central")
-    download("http://search.maven.org/remotecontent?filepath=org/apache/tika/tika-app/1.10/tika-app-1.10.jar", tika_jar)
+    info("  Downloading tika-app-1.17.jar from Maven Central")
+    download("http://search.maven.org/remotecontent?filepath=org/apache/tika/tika-app/1.17/tika-app-1.17.jar", tika_jar)
 end
 
-fop_jar = joinpath(tdeps, "fop-2,0", "fop-2.0.jar")
-fop_gz = joinpath(tdeps, "fop-2.0-bin.tar.gz")
+fop_jar = joinpath(tdeps, "fop-2,2", "fop-2.2.jar")
+fop_gz = joinpath(tdeps, "fop-2.2-bin.tar.gz")
 
 if !isfile(fop_gz)
-    info("  Downloading fop-2.0 binary from Apache OSUOSL Mirror")
+    info("  Downloading fop-2.2 binary from Apache OSUOSL Mirror")
     download("http://apache.osuosl.org/xmlgraphics/fop/binaries/fop-2.0-bin.tar.gz", fop_gz)
 end
 if !isfile(fop_jar)
