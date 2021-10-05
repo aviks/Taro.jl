b4614ca563e6a657447b745bc8c7a809cadb40cc
José Bayoán Santiago Calderón, PhD <naoyabpr@gmail.com>
2021-10-05 11:10:23 -0400

    Apache OSUOSL binaries from v2.3 to v2.6
    Fixes currently broken installation.

--- deps/build.jl
+++ deps/build.jl
@@ -5,14 +5,14 @@ if !isfile(tika_jar)
     download("https://archive.apache.org/dist/tika/tika-app-1.23.jar", tika_jar)
 end
 
-fop_dir = joinpath(tdeps, "fop-2.3", "fop")
-fop_jar = joinpath(fop_dir, "build", "fop-2.3.jar")
+fop_dir = joinpath(tdeps, "fop-2.6", "fop")
+fop_jar = joinpath(fop_dir, "build", "fop-2.6.jar")
 fop_lib = joinpath(fop_dir, "lib" )
-fop_gz = joinpath(tdeps, "fop-2.3-bin.tar.gz")
+fop_gz = joinpath(tdeps, "fop-2.6-bin.tar.gz")
 
 if !isfile(fop_gz)
-    @info "  Downloading fop-2.3 binary from Apache OSUOSL Mirror"
-    download("https://apache.osuosl.org/xmlgraphics/fop/binaries/fop-2.3-bin.tar.gz", fop_gz)
+    @info "  Downloading fop-2.6 binary from Apache OSUOSL Mirror"
+    download("https://apache.osuosl.org/xmlgraphics/fop/binaries/fop-2.6-bin.tar.gz", fop_gz)
 end
 if !isfile(fop_jar)
     if Sys.isunix() unpack_cmd = `tar xzf $fop_gz --directory=$tdeps` end
